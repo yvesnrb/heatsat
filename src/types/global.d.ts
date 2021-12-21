52861afa89896b221706e4a05c3b8d59bafc53c0
Yves Bode <ynrbode@gmail.com>
2021-12-21 20:50:21 -0300

    fix: fix use of global variable on mongodb client

--- src/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-declare module globalThis {
-  let _mongoClient: Promise<MongoClient>;
-}
--- src/util/mongodb.ts
+++ src/util/mongodb.ts
@@ -11,19 +11,8 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClient) {
-    client = new MongoClient(mongoConfig.uri, options);
-    global._mongoClient = client.connect();
-  }
-  mongoClientPromise = global._mongoClient;
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(mongoConfig.uri, options);
-  mongoClientPromise = client.connect();
-}
+client = new MongoClient(mongoConfig.uri, options);
+mongoClientPromise = client.connect();
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
