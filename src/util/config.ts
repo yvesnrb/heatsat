816b9c6c5f12265eb3e5ee03b713f45af8cf677d
Yves Bode <ynrbode@gmail.com>
2021-12-21 20:25:57 -0300

    feat: implement a config file with data from environment variables

---
+++ src/util/config.ts
@@ -0,0 +1,13 @@
+export const mongoConfig = {
+  uri: process.env.MONGODB_URI || '',
+};
+
+export const inpeConfig = {
+  url: process.env.INPE_URL || '',
+  user: process.env.INPE_USER || '',
+  pass: process.env.INPE_PASS || '',
+};
+
+export const mapsConfig = {
+  key: process.env.MAPS_API_KEY || '',
+};
